5b8826f395e7b0ee5465ac8037e46adfbd4a203a
Benjamin Jesuiter <bjesuiter@gmail.com>
2023-07-24 20:46:58 +0200

    update layout bg color

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -7,7 +7,7 @@ type LayoutProps = {
 
 export function Layout(props: LayoutProps) {
   return (
-    <div class="min-h-[100dvh] bg-slate-100 dark:bg-slate-800 text-slate-900 dark:text-slate-100">
+    <div class="min-h-[100dvh] bg-slate-100 dark:bg-slate-700 text-slate-900 dark:text-slate-100">
       <header>
         <MainNav></MainNav>
       </header>
