702085ccf4a48d909b690eb558e7fdee4d76590f
Benjamin Jesuiter <bjesuiter@gmail.com>
2023-07-24 19:42:12 +0200

    add some tailwind presets

--- twind.config.ts
+++ twind.config.ts
@@ -1,10 +1,17 @@
 import { defineConfig, Preset } from "@twind/core";
 import presetTailwind from "@twind/preset-tailwind";
 import presetAutoprefix from "@twind/preset-autoprefix";
+import presetContainerQueries from "@twind/preset-container-queries";
+import presetTypography from "@twind/preset-typography";
 
 export default {
   ...defineConfig({
-    presets: [presetTailwind() as Preset, presetAutoprefix()],
+    presets: [
+      presetAutoprefix(),
+      presetTailwind() as Preset,
+      presetContainerQueries(),
+      presetTypography(),
+    ],
   }),
   selfURL: import.meta.url,
 };
